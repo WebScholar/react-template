cbfc64df262a42c2feb43708967cab68f2d79436
Smart <807996636@qq.com>
2020-09-23 11:27:47 +0800

    index.css

---
+++ src/pages/Main/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Style from './index.module.less'
+
+function Main() {
+  return (
+    <div className={Style.container}>
+    </div>
+  );
+}
+
+export default Main;
